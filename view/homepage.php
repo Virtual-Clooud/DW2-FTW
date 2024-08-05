18b4f4fdf229477235a6a496e114ce5329dcd88a
Alexandre <goulart726@gmail.com>
2024-08-05 16:24:56 -0300

    added styles.css, modif homepage.php

--- view/homepage.php
+++ view/homepage.php
@@ -9,27 +9,42 @@
 <html>
       <head>
             <meta charset="utf-8">
+            <link rel="stylesheet" href="styles.css">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>HOME</title>
       </head>
       <body>
-            <h3>Olá, <?php echo $_SESSION['nome']; ?></h1>
-            <div style="width:800px; margin:0 auto; text-align: center;"><!-- Centralizar tudo aq -->
+
+            <div class="cabecalho">
+                  <h1>Meu Site</h1>
+                  <nav>
+                        <ul>
+                               <li><a href="#">Olá, <?php echo $_SESSION['nome']; ?></a></li>
+                               <li><a href="#">Sobre</a></li>
+                               <li><a href="#">Contato</a></li>
+                        </ul>
+                  </nav>
+            </div>
+            
+            <div class="conteudo"><!-- Centralizar tudo aq -->
                   <h1>Diagrama</h1>
                   <h2>Crie diagramas que o ajudarão á tomar as decisões importantes</h2>
                   <br>
-                  <br>
-                  <br>
-                  <br> 
+                   
                   <br><br><br><br>
-                  <h3> Comece a criar! </h3>
-                  <button type="button">Click Me!</button>
+                  <h3>  </h3>
+                  <a class="botao" href="">Comece a criar!</a>
+
+                  
     
+             </div>
+             <div class="rodape">
+                   <a class = "botao" href="../Controller/logout.php">Logout</a>
              </div>
     
              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-            <a href="../Controller/logout.php">Logout</a>
+            
 
       </body>
 </html>
